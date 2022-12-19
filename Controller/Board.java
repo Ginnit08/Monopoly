b3824487083cf5aa145cde5e0d51d661d8c3e8bf
Thanh Nhat Tran <94516519+Ginnit08@users.noreply.github.com>
2022-12-19 13:55:24 +0700

    update code

--- Controller/Board.java
+++ Controller/Board.java
@@ -1,26 +1,33 @@
 package Controller;
+
+import javax.swing.JFrame;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.*; 
 
 public class Board extends JFrame {
-    private Image imgB;
     public Board() {
+        setTitle("Monopoly");
+        setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+        BufferedImage bufferedImage = null;
         try {
-            imgB = ImageIO.read(Objects.requireNonNull(getClass().getResource("img/Board.jpg")));
-        } catch (Exception e) {
+            bufferedImage = ImageIO.read(new File("img/Board.jpg"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        //Drawing the board
+
+        Image image = bufferedImage.getScaledInstance(1536, 864, Image.SCALE_DEFAULT);
+        ImageIcon icon = new ImageIcon(image);
+        JLabel jLabel = new JLabel();
+        jLabel.setIcon(icon);
+        add(jLabel);
+        this.setVisible(true);
     }
 
-    @Override
-    public void paint(Graphics g) {
-        int w = getWidth();
-        int h = getHeight();
-
-        Graphics2D graphics2D = (Graphics2D) g;
-        g.drawImage(imgB,0,0,2*w/3,h,this);
-    }
 }
