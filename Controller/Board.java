150108e845ded87b51b37dab37ef116628f0a6bf
Thanh Nhat Tran <94516519+Ginnit08@users.noreply.github.com>
2022-12-26 19:45:08 +0700

    update code

--- Controller/Board.java
+++ Controller/Board.java
@@ -2,6 +2,8 @@ package Controller;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
@@ -43,17 +45,36 @@ public class Board extends JFrame {
 
         // Game running
         JPanel game = new JPanel();
+        Dice dice1 = new Dice(screenHeight + 50, 50, 50, 50);
+        game.add(dice1);
+
+        Dice dice2 = new Dice(screenHeight + 150, 50, 50, 50);
+        game.add(dice2);
         game.setBounds(screenHeight, 0, (screenWidth - screenHeight) / 2, screenHeight);
         game.setBackground(Color.GRAY);
 
         // game info 
         JPanel info = new JPanel();
         info.setBounds(screenHeight + (screenWidth - screenHeight) / 2, 0, (screenWidth - screenHeight) / 2, screenHeight);
-        info.setBackground(Color.PINK);
+
+        JPanel infoPlayer1 = new JPanel();
+		infoPlayer1.setBorder(new LineBorder(new Color(0, 0, 0)));
+        infoPlayer1.setBounds(screenHeight + (screenWidth - screenHeight) / 2, 0, (screenWidth - screenHeight) / 2, screenHeight/2 - 30);
+        infoPlayer1.add(new Player(1, Color.RED));
+
+        JPanel infoPlayer2 = new JPanel();
+		infoPlayer2.setBorder(new LineBorder(new Color(0, 0, 0)));
+        infoPlayer2.setBounds(screenHeight + (screenWidth - screenHeight) / 2, infoPlayer1.getHeight() , (screenWidth - screenHeight) / 2, screenHeight/2);
+        infoPlayer2.add(new Player(2, Color.BLUE));
+
+
+
+        add(infoPlayer1);
+        add(infoPlayer2);
 
         add(boardGame);
         add(game);
-        add(info);
+        // add(info);
 
         this.setVisible(true);
     }
@@ -315,5 +336,4 @@ public class Board extends JFrame {
     public void paintComponent(Graphics g) {
         super.paintComponents(g);
     }
-
 }
--- Controller/Main.java
+++ Controller/Main.java
@@ -1,7 +1,16 @@
 package Controller;
 
+import java.awt.Color;
+
+import javax.swing.*;
+
 public class Main {
     MainFrame frame;
+	static int nowPlaying = 0;
+	static JTextArea infoConsole;
+    Player player1 = new Player(1, Color.RED);
+    Player player2 = new Player(2, Color.BLUE);
+
     
     public Main() {
         frame = new MainFrame();
